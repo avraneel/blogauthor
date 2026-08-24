f328c66d617aa31b0c520512af697122b10bdf14
Avraneel Pal <avraneelpal22@gmail.com>
2026-08-24 09:01:58 +0530

    add create post

--- src/components/Create.jsx
+++ src/components/Create.jsx
@@ -1,12 +1,30 @@
 import styles from "../css/authForm.module.css";
 
 export default function Create() {
+  async function handleSubmit(formData) {
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    myHeaders.append("Authorization", localStorage.getItem("token"));
+    console.log(formData);
+    const request = {
+      method: "POST",
+      headers: myHeaders,
+      body: JSON.stringify({
+        title: formData.get("title"),
+        content: formData.get("content"),
+      }),
+    };
+
+    const response = await fetch("http://localhost:3000/posts", request);
+    const data = await response.json();
+  }
+
   return (
     <div className={styles.formContainer}>
       <h2>Create a Post</h2>
-      <form action="" className={styles.formBody}>
+      <form action={handleSubmit} className={styles.formBody}>
         <div className={styles.formItem}>
-          <label htmlFor="ttle">Title</label>
+          <label htmlFor="title">Title</label>
           <input type="text" name="title" id="title" />
         </div>
         <div className={styles.formItem}>
