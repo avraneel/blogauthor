5870463f74db07319fd13187dcb2be05a3a7576e
Avraneel Pal <avraneelpal22@gmail.com>
2026-08-26 22:31:00 +0530

    update post list to include delete button

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,13 +1,14 @@
 import { Link } from "react-router";
 import { useState, useEffect } from "react";
-import { useNavigate } from "react-router";
+import { useNavigate, useOutletContext } from "react-router";
 
 export default function PostList() {
+  const [session, setSession] = useOutletContext();
   const navigate = useNavigate();
   const [postTitles, setPostTitles] = useState([]);
 
   async function getData() {
-    const response = await fetch("http://localhost:3000/posts");
+    const response = await fetch("http://localhost:3000/posts?published=true");
     const data = await response.json();
 
     setPostTitles(data);
@@ -35,17 +36,41 @@ export default function PostList() {
     getData();
   }
 
+  async function handleDelete() {
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    myHeaders.append("Authorization", localStorage.getItem("token"));
+    const request = {
+      method: "DELETE",
+      headers: myHeaders,
+    };
+    const response = await fetch(
+      `http://localhost:3000/posts/${postId}`,
+      request,
+    );
+    await response.json();
+    getData();
+  }
+
   const listElements = postTitles.map((post) => (
     <li key={post.id}>
       <Link to={`posts/${post.id}`}>{post.title}</Link>
-      <form action={(formData) => handleSubmit(post.id, formData)}>
-        <input
-          type="hidden"
-          name="published"
-          value={post.published ? false : true}
-        />
-        <button>{post.published ? "Unpublish" : "Publish"}</button>
-      </form>
+      {session && (
+        <div>
+          <form action={(formData) => handleSubmit(post.id, formData)}>
+            <input
+              type="hidden"
+              name="published"
+              value={post.published ? false : true}
+            />
+            <button>{post.published ? "Unpublish" : "Publish"}</button>
+          </form>
+          <form action={handleDelete}>
+            <input type="hidden" name="delete" value={post.id} />
+            <button>Delete</button>
+          </form>
+        </div>
+      )}
     </li>
   ));
 
